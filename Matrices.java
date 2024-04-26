eb0cbf2ddfa5f32aaedaaa005afe96dc832ffcb2
SIDEDx <160801681+SIDEDx@users.noreply.github.com>
2024-04-26 00:53:34 -0400

    Update Matrices.java

--- Matrices.java
+++ Matrices.java
@@ -14,4 +14,14 @@ public class Matrices {
         }
         return C;
     }
+
+    public static int[][] transposeMatrix(int[][] A) {
+        int[][] AT = new int[2][2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                AT[i][j] = A[j][i];
+            }
+        }
+        return AT;
+    }
 }
