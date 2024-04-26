84b7c6062971126eda7c4c636f042d09f66537da
ChaynerSclp <168158667+ChaynerSclp@users.noreply.github.com>
2024-04-26 00:49:49 -0400

    Update Matrices.java

--- Matrices.java
+++ Matrices.java
@@ -2,4 +2,16 @@ public class Matrices {
     public static void main(String[] args) {
 
     }
+
+    public static int[][] multiplyMatrices(int[][] A, int[][] B) {
+        int[][] C = new int[2][2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        return C;
+    }
 }
