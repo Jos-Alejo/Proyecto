04d26dd09b7176e3ae673d60d5512dac6664c994
SaimariAlpizarVivez <168163107+SaimariAlpizarVivez@users.noreply.github.com>
2024-04-26 01:33:10 -0400

    Update Matrices.java

--- Matrices.java
+++ Matrices.java
@@ -45,4 +45,24 @@ public static int[][] addMatrices(int[][] A, int[][] B) {
         }
         return AT;
     }
+
+    public static int[][] inverseMatrix(int[][] A) {
+        int det = A[0][0] * A[1][1] - A[0][1] * A[1][0];
+        int[][] inverseA = {{A[1][1], -A[0][1]}, {-A[1][0], A[0][0]}};
+
+        if(det!=0){
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    inverseA[i][j] /= det;
+                }
+            }
+            
+        }else{
+            System.out.println("El determinante de la matriz A es 0, por lo que no es posible realizar la operación.");
+            System.exit(0);   
+        }
+        
+        return inverseA;
+        
+    }
 }
